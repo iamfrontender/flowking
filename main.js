2ac1ceca4bdd5885e9d9d7aad7d32cf33736f60e
Ivan Cherepanov <iamfrontender@gmail.com>
2017-06-13 11:17:10 +0300

    finishing touches

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 'use strict';
 
+/**
+ * Defines the shape of a boid
+ * @type {Array<Number[]>} - array of position vectors
+ */
+const cursor = [
+    [-14, 6],
+    [-12, 1],
+    [-22, 1],
+    [-22, -1],
+    [-12, -1],
+    [-14, -6],
+    [0, 0]
+];
+
+/**
+ * Holds helper utilities
+ */
 const utils = {
+
+    /**
+     * Creates random float number within given interval
+     *
+     * @param {Number} bottom
+     * @param {Number} top
+     * @return {Number}
+     */
     rand(bottom, top) {
         if (top === undefined) {
             top = bottom;
@@ -10,50 +35,40 @@ const utils = {
         return bottom + (Math.random() * (top - bottom));
     },
 
+    /**
+     * Double-dog-leg hypothenuse approximation
+     *
+     * @param {Number} a
+     * @param {Number} b
+     * @return {Number} c - hypothenuse length
+     */
     hypot(a, b) {
         a = Math.abs(a);
         b = Math.abs(b);
         let lo = Math.min(a, b);
         let hi = Math.max(a, b);
         return hi + 3 * lo / 32 + Math.max(0, 2 * lo - hi) / 8 + Math.max(0, 4 * lo - hi) / 16;
+    },
+
+    /**
+     * Normalizes given vector
+     *
+     * @param {Number} x
+     * @param {Number} y
+     * @return {Number[]}
+     */
+    normalize([x, y]) {
+        let length = Math.sqrt(x * x + y * y);
+        return [x / length, y / length];
     }
 };
 
-class EventEmitter {
-    static wrap(entity) {
-        let emitter = new EventEmitter();
-
-        entity.on = emitter.on;
-        entity.emit = emitter.emit;
-        entity.listeners = emitter.listeners;
-    }
-
-    constructor() {
-        this.listeners = {};
-    }
-
-    on(event, listener) {
-        this.listeners[event] = this.listeners[event] || [];
-        this.listeners[event].push(listener);
-    }
-
-    emit(event, ...args) {
-        if (this.listeners[event]) {
-            this.listeners[event].forEach(listener => {
-                try {
-                    listener(...args);
-                } catch(e) {
-                    console.error(`EventEmitter: event ${event} failed:`, e);
-                }
-            });
-        }
-    }
-}
-
+/**
+ * Implements boids flocking behavior which
+ * could be then rendered
+ */
 class Boids {
     constructor(opts = {}) {
-        EventEmitter.wrap(this);
-
         this.speedLimitRoot = opts.speedLimit || 0;
         this.accelerationLimitRoot = opts.accelerationLimit || 1;
         this.speedLimit = Math.pow(this.speedLimitRoot, 2);
@@ -65,13 +80,17 @@ class Boids {
         this.cohesionForce = opts.cohesionForce || 0.1;
         this.alignmentForce = opts.alignmentForce || opts.alignment || 0.25;
         this.attractors = opts.attractors || [];
-        this.cb = opts.cb || (() => {});
+
+        let quarterWidth = window.innerWidth / 4;
+        let quarterHeight = window.innerHeight / 4;
 
         this.boids = Array(opts.boids || 50)
             .fill(0)
             .map(() => ({
-                speed: [utils.rand(-25, 0), utils.rand(-25, 0)],
-                position: [utils.rand(-window.innerWidth / 4, window.innerWidth / 4), utils.rand(-window.innerHeight / 4, window.innerHeight / 4)],
+                speed: [
+                    utils.rand(-25, 25), utils.rand(-25, 25)
+                ],
+                position: [utils.rand(-quarterWidth, quarterWidth), utils.rand(-quarterHeight, quarterHeight)],
                 acceleration: [0, 0]
             }));
     }
@@ -93,7 +112,7 @@ class Boids {
                 ];
                 let dist = (spareX * spareX) + (spareY * spareY);
 
-                if (dist < radius) {
+                if (dist < radius * radius) {
                     let length = utils.hypot(spareX, spareY);
                     boid.speed[0] -= force * spareX / length || 0;
                     boid.speed[1] -= force * spareY / length || 0;
@@ -163,40 +182,65 @@ class Boids {
             boid.position[0] += boid.speed[0];
             boid.position[1] += boid.speed[1];
         });
+    }
 
-        this.cb(this.boids);
+    draw() {
+        this.boids.forEach((boid) => {
+            let [x, y] = boid.position;
+            let halves = flow.halves;
+
+            boid.position[0] = x > halves.width ? -halves.width : -x > halves.width ? halves.width : x;
+            boid.position[1] = y > halves.height ? -halves.height : -y > halves.height ? halves.height : y;
+
+            drawCursor(boid);
+        });
+    }
+}
+
+/**
+ * Implements uber-precise chronodisplay
+ */
+class Clocks {
+    constructor(selector) {
+        this.el = document.querySelector(selector);
+    }
+
+    run() {
+        this.tick();
+        setInterval(() => this.tick(), 5000);
+    }
+
+    tick() {
+        let now = new Date();
+        this.el.textContent = `${now.getHours() % 12}:${now.getMinutes()} ${now.getHours() >= 12 ? 'PM' : 'AM'}`;
     }
 }
 
 ////////////////////////////////////////////////////////////////////////
 
+const ctx = flow.getContext('2d');
 const boids = new Boids({
-    boids: 100,              // The amount of boids to use
+    boids: 140,            // The amount of boids to use
     speedLimit: 2,          // Max steps to take per tick
     accelerationLimit: .05,   // Max acceleration per tick
     separationDistance: 60, // Radius at which boids avoid others
-    alignmentDistance: 180, // Radius at which boids align with others
-    cohesionDistance: 300,  // Radius at which boids approach others
-    separationForce: .15,  // Speed to avoid at
-    alignmentForce: .25,   // Speed to align with other boids
-    cohesionForce: .1,     // Speed to move towards other boids
-    attractors: [
-        [
-            Infinity,
-            Infinity,
-            50000,
-            .1
-        ]
-    ]
+    alignmentDistance: 1000, // Radius at which boids align with others
+    cohesionDistance: 1000,  // Radius at which boids approach others
+    separationForce: 50,  // Speed to avoid at
+    alignmentForce: 50,   // Speed to align with other boids
+    cohesionForce: 50,     // Speed to move towards other boids
+    attractors: []
 });
-
-const ctx = flow.getContext('2d');
+const clocks = new Clocks('#time');
 
 window.addEventListener('resize', onResize);
+document.addEventListener('mouseenter', onMouseEnter);
 document.addEventListener('mousemove', onMousemove);
+document.addEventListener('mouseleave', onMouseLeave);
 
 onResize();
 run();
+clocks.run();
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -205,21 +249,10 @@ function run() {
     ctx.fillStyle = 'white';
 
     ctx.save();
-    ctx.translate(flow.width/2, flow.height/2);
+    ctx.translate(flow.halves.width, flow.halves.height);
 
     boids.tick();
-    boids.boids.forEach((boid) => {
-        let [x, y] = boid.position;
-        let halves = [
-            flow.width / 2,
-            flow.height / 2
-        ];
-
-        boid.position[0] = x > halves[0] ? -halves[0] : -x > halves[0] ? halves[0] : x;
-        boid.position[1] = y > halves[1] ? -halves[1] : -y > halves[1] ? halves[1] : y;
-
-        drawCursor(boid);
-    });
+    boids.draw(ctx);
 
     ctx.restore();
 
@@ -229,61 +262,49 @@ function run() {
 function onResize() {
     flow.width = window.innerWidth;
     flow.height = window.innerHeight;
+    flow.halves = {
+        width: flow.width / 2,
+        height: flow.height / 2
+    };
+}
+
+function onMouseEnter(e) {
+    boids.attractors.push([
+        e.x, e.y, 150, .4
+    ]);
+}
+
+function onMouseLeave() {
+    boids.attractors.shift();
 }
 
 function onMousemove(e) {
-    boids.attractors[0][0] = e.x - (flow.width / 2);
-    boids.attractors[0][1] = e.y - (flow.height / 2);
+    boids.attractors[0][0] = e.x - flow.halves.width;
+    boids.attractors[0][1] = e.y - flow.halves.height;
 }
 
 function drawCursor(boid) {
     let position = boid.position;
-    let direction = normalize(boid.speed);
-    const cursor = [
-        [-14, 6],
-        [-12, 1],
-        [-22, 1],
-        [-22, -1],
-        [-12, -1],
-        [-14, -6],
-        [0, 0]
-    ];
+    let direction = utils.normalize(boid.speed);
 
     direction = Math.atan2(direction[0], direction[1]);
 
     ctx.strokeStyle = 'black';
     ctx.strokeWidth = '2px';
-
     ctx.fillStyle = 'white';
 
-    ctx.save();
     ctx.beginPath();
 
     let cos = Math.cos(direction);
     let sin = Math.sin(direction);
-
     let coords = cursor.map(([x, y]) => ([x * sin - y * cos, x * cos + y * sin]));
 
-    ctx.moveTo(
-        position[0],
-        position[1]
-    );
-
+    ctx.moveTo(position[0], position[1]);
     coords.forEach(([x, y]) => {
         ctx.lineTo(position[0] + x, position[1] + y);
     });
 
     ctx.fill();
     ctx.stroke();
-
-    ctx.restore()
 }
 
-function normalize([x, y]) {
-    let length = Math.sqrt(x * x + y * y);
-
-    return [
-        x / length,
-        y / length
-    ];
-}
